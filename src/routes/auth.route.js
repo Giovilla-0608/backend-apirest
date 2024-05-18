edeac2a344fcdfd2de9239c0943d7ab8e34c6784
--help <giovannivilla0024@gmail.com>
2024-05-17 23:21:54 -0600

    cree las rutas para la validacion de toda la ejecucion de servicios

---
+++ src/routes/auth.route.js
@@ -0,0 +1,13 @@
+import { Router } from "express"
+import { registro, login, logout, profile } from '../controllers/user.controller.js';
+import { authRequired } from '../middlewares/validateToken.js'
+
+const router = Router();
+
+//peticiones
+router.post('/registrer', registro)
+router.post('/login', login)
+router.post('/logout', logout)
+router.get('/profile', authRequired, profile)
+
+export default router;
