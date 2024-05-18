44de4c8378228211adba526f2b4cfe8cd7f681a8
--help <giovannivilla0024@gmail.com>
2024-05-17 23:06:05 -0600

    creelas respuesta de login, logout y profile para las respuesta en el servidor como en el cliente y se configuro todo para su funcionamiento

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,5 +34,50 @@ export const registro = async(req, res)=>{
 }
 
 export const login = async(req, res)=>{
-    
+    const {email, password} = req.body;
+
+    try {
+        // guardamos en la variable el usuario o email que encontramos en la base de datos
+        const userFound = await User.findOne({email})
+        //hacemos una validacion
+        if(!userFound) return res.status(400).json(["user not found"])
+
+        //logica para verificar el password del usuario con el password en la base de datos del email que encontramos
+        const isMatch = await bcrypt.compare(password, userFound.password)
+        //hacemos una validacion
+        if(!isMatch) return res.status(400).json(["incorrect password"])
+        //utilizar el token
+        const token = await createAccessToken({id: userFound._id})//enviamos el id para que lo cree como un token
+        //crea una cookie que ya viene de express, para que se cree directamente la cookie en nuestro navegador
+        res.cookie("token", token)
+        
+        //respuesta al cliente
+        res.json({
+            email:userFound.email,
+            username: userFound.username,
+            id: userFound.id,
+            createdAt: userFound.createdAt
+        })
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+}
+
+export const logout = (req, res) => {
+    res.cookie("token", "", {
+        expires: new Date(0),
+    });
+    return res.sendStatus(200)
+}
+
+export const profile = async(req, res) => {
+    const userFound = await User.findById(req.user.id)
+
+    if(!userFound) return res.status(400).json(["user not found"]);
+
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+    })
 }
